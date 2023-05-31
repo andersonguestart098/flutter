e225b699a8737fd48fba873bef578a8713635721
anderson.guestart098 <anderson.guestart98@gmail.com>
2023-05-31 16:29:25 -0300

    https://github.com/andersonguestart098/flutter.git

---
+++ estudando_flutter/lib/main.dart
@@ -0,0 +1,101 @@
+
+import 'package:estudando_flutter/main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+
+  String abc = "olá";
+
+  MyApp({super.key});
+  
+  get image => null;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      home: Scaffold(
+        appBar:
+       AppBar(
+        backgroundColor: Color.fromARGB(255, 0, 3, 5),
+        leading: Center(child: Image(image: NetworkImage("https://lh3.googleusercontent.com/drive-viewer/AFGJ81p2PEH6utFlhyLBNHhJ1ng86ZzBtIY27ayWAPREphiOko4JzJgP57R4z5bHB-8cUmnl2qaGw8N-NaINaRf5kr9Oih2o=w1366-h616"))),),
+       backgroundColor: Color.fromARGB(255, 255, 254, 254),
+      body: Column(
+        children: [
+          Column(
+            children: [
+            Title(color: Color.fromARGB(255, 0, 3, 5), child: Text("Piso Vinilico Forbo 50x50")),
+              Row(
+                children: [
+                  Center(child: Image(image: NetworkImage('https://forbo.azureedge.net/forboimages/1226401/Sphera-Element_51037_50070_50071_HIGH.jpg'),width: 150, height: 80,)),
+                  
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    ElevatedButton(
+                    child: const Text("Mostrar detalhes"),
+                     onPressed: () {
+                      showBarModalBottomSheet(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return SizedBox(
+                            height: 400,
+                            child: Center(
+                              child: ElevatedButton(
+                                child: const Text('close'),
+                                onPressed: (){
+                                  Navigator.pop(context);
+                                },
+                                ),
+                            ),
+                          );
+                        },
+                        );
+                     },
+                     ),
+                  ],
+                ),
+              ),
+              Title(color: Colors.white, child: Text("Piso Vinilico Forbo 100x100"),),
+              Row(
+                children: [
+                  Center(child: Image(image: NetworkImage('https://www.neointeriores.com.br/wp-content/uploads/2022/09/forbo.jpg'),width: 150, height: 97,)),
+                ],
+                
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    ElevatedButton(onPressed: (){}, child: Text("Mostrar Detalhes...")),
+                  ],
+                ),
+                
+              )
+            
+            ],
+          ),
+        ],
+      ),
+      
+      bottomNavigationBar: SizedBox(child: const AboutListTile())
+      ),
+      
+    );
+    
+  }
+  
+
+}
